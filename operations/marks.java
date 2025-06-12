c103c285ed8b1243194294b2872807e94c86663d
Devadharshini-S-2712 <dharshini.bcc@gmail.com>
2025-06-12 15:58:02 +0530

    simple math func

---
+++ operations/marks.java
@@ -0,0 +1,10 @@
+package operations;
+
+public class marks {
+    public static void main(String[] args) {
+        int a=96;
+        int b=90;
+        int c=99;
+        System.out.println(a+b+c);
+    }
+}
