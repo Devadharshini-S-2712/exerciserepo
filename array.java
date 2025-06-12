2acda57aa4b19c41b05a450c9842cce45732dbcf
Devadharshini-S-2712 <dharshini.bcc@gmail.com>
2025-06-12 08:59:49 +0530

    changed the class name

--- array.java
+++ array.java
@@ -1,4 +1,4 @@
-public class main{
+public class array{
   public static void main(String args[]){
     System.out.println("hello everyone");
     System.out.println("This is first file of my repo");
