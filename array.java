303ac8f079e8976edbb252ed55fcb709226dd66d
Devadharshini-S-2712 <dharshini.bcc@gmail.com>
2025-06-12 08:46:47 +0530

    Update array.java

--- array.java
+++ array.java
@@ -1,5 +1,6 @@
 public class main{
   public static void main(String args[]){
-    System.out.println("hello everyone")
+    System.out.println("hello everyone");
+    System.out.println("This is first file of my repo");
       }
 }
