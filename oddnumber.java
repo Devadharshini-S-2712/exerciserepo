8ebff9ec540862ec701255a0add69f47d701c8d0
Devadharshini-S-2712 <dharshini.bcc@gmail.com>
2025-06-12 09:14:22 +0530

    oddnumber file created

---
+++ oddnumber.java
@@ -0,0 +1,9 @@
+public class oddnumber {
+    public static void main(String[] args) {
+       for(int num=1;num<=100;num++){
+        if(num%2==1){
+            System.out.println(num);
+        }
+       }
+    }
+}
